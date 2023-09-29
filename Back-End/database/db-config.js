ae6d9968c5ca058675cbd575b25d1b382f61932c
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-09-29 12:01:12 -0500

    Update db-config.js

--- Back-End/database/db-config.js
+++ Back-End/database/db-config.js
@@ -1,1 +1,2 @@
-//Database connection and configuration.
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://jamesfairley505:Qwerre123@cluster0.tbcmvdy.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
