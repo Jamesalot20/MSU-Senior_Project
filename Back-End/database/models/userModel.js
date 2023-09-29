abcb441e220a490b9f15cfa715959feb3773fb2b
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-09-29 12:19:20 -0500

    Update userModel.js

--- Back-End/database/models/userModel.js
+++ Back-End/database/models/userModel.js
@@ -1,1 +1,25 @@
-//model/schema for a User.
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true
+    },
+    Email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    Password: {
+        type: String,
+        required: true
+    },
+    RegistrationDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
