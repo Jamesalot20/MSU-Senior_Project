971ddeb21eb221c8b2d52482585cc4fa82c8d70f
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-10-20 12:27:33 -0500

    Update userFormModel.test.js

--- Back-End/database/models/userFormModel.test.js
+++ Back-End/database/models/userFormModel.test.js
@@ -1,9 +1,10 @@
 const UserForm = require('./userFormModel');
+const mongoose = require('mongoose');
 
 test('should be truthy for valid user form', () => {
     const userForm = new UserForm({
-        UserID: '60d3b41f8baf7c35f042f3d8',  // Sample ObjectId (use a valid one)
-        TemplateID: '60d3b41f8baf7c35f042f3d9',  // Sample ObjectId (use a valid one)
+        UserID: '60d3b41f8baf7c35f042f3d8',
+        TemplateID: '60d3b41f8baf7c35f042f3d9',
         FormName: 'Registration Form',
         FormStructure: {
             field1: 'Text Input',
@@ -18,4 +19,56 @@ test('should be truthy for valid user form', () => {
         field1: 'Text Input',
         field2: 'Dropdown'
     });
+});
+
+test('should be falsy for an invalid UserID or TemplateID', async () => {
+    const userForm = new UserForm({
+        UserID: 'invalidId',
+        TemplateID: '60d3b41f8baf7c35f042f3d9',
+        FormName: 'Registration Form',
+        FormStructure: {
+            field1: 'Text Input',
+            field2: 'Dropdown'
+        }
+    });
+
+    try {
+        await userForm.validate();  
+        expect(true).toBe(false);
+    } catch (err) {
+        expect(err.errors['UserID']).toBeDefined();
+    }
+});
+
+test('should be falsy for missing FormName', async () => {
+    const userForm = new UserForm({
+        UserID: '60d3b41f8baf7c35f042f3d8',
+        TemplateID: '60d3b41f8baf7c35f042f3d9',
+        FormStructure: {
+            field1: 'Text Input',
+            field2: 'Dropdown'
+        }
+    });
+
+    try {
+        await userForm.validate();
+        expect(true).toBe(false);
+    } catch (err) {
+        expect(err.errors['FormName']).toBeDefined();
+    }
+});
+
+test('should be falsy for missing FormStructure', async () => {
+    const userForm = new UserForm({
+        UserID: '60d3b41f8baf7c35f042f3d8',
+        TemplateID: '60d3b41f8baf7c35f042f3d9',
+        FormName: 'Registration Form'
+    });
+
+    try {
+        await userForm.validate();
+        expect(true).toBe(false);
+    } catch (err) {
+        expect(err.errors['FormStructure']).toBeDefined();
+    }
 });
