bb7d7560ea7094a3b0430af0fa5fc167d29f2fcc
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-10-20 13:15:34 -0500

    Update formTemplateRoutes.js

--- Middle-End/api/routes/formTemplateRoutes.js
+++ Middle-End/api/routes/formTemplateRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const FormTemplate = require('../database/models/formTemplateModel');
+const FormTemplate = require('../../../Back-End/database/models/formTemplateModel');
 
 // CREATE
 router.post('/form-template', async (req, res) => {
