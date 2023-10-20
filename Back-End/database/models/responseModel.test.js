e07474c09078cc381190b68a21e6cc1066996f1e
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-10-20 12:43:17 -0500

    Update responseModel.test.js

--- Back-End/database/models/responseModel.test.js
+++ Back-End/database/models/responseModel.test.js
@@ -1,12 +1,12 @@
 const Response = require('./responseModel');
+const mongoose = require('mongoose');
 
 test('should be truthy for a valid response', () => {
     const response = new Response({
-        UserFormID: '60d3b41f8baf7c35f042f3d8',  // Sample ObjectId (use a valid one)
+        UserFormID: '60d3b41f8baf7c35f042f3d8',
         SubmissionData: {
             question1: 'Answer for question 1',
             question2: 'Answer for question 2'
-
         }
     });
 
@@ -14,6 +14,35 @@ test('should be truthy for a valid response', () => {
     expect(response.SubmissionData).toEqual({
         question1: 'Answer for question 1',
         question2: 'Answer for question 2'
-
     });
+});
+
+test('should be falsy for an invalid UserFormID', async () => {
+    const response = new Response({
+        UserFormID: 'invalidId',
+        SubmissionData: {
+            question1: 'Answer',
+            question2: 'Another Answer'
+        }
+    });
+
+    try {
+        await response.validate();
+        expect(true).toBe(false); // This line should not be reached if validation throws an error
+    } catch (err) {
+        expect(err.errors['UserFormID']).toBeDefined();
+    }
+});
+
+test('should be falsy for missing SubmissionData', async () => {
+    const response = new Response({
+        UserFormID: '60d3b41f8baf7c35f042f3d8'
+    });
+
+    try {
+        await response.validate();
+        expect(true).toBe(false); // This line should not be reached if validation throws an error
+    } catch (err) {
+        expect(err.errors['SubmissionData']).toBeDefined();
+    }
 });
