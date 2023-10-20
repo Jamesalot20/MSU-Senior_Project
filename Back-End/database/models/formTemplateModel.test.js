dfd0066979077388690e26a653c1116a96add50a
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-10-20 12:44:46 -0500

    Update formTemplateModel.test.js

--- Back-End/database/models/formTemplateModel.test.js
+++ Back-End/database/models/formTemplateModel.test.js
@@ -17,3 +17,31 @@ test('should be truthy for a valid form template', () => {
         field3: 'Checkbox'
     });
 });
+
+test('should be falsy for missing TemplateName', async () => {
+    const formTemplate = new FormTemplate({
+        TemplateStructure: {
+            field1: 'Text Area'
+        }
+    });
+
+    try {
+        await formTemplate.validate();
+        expect(true).toBe(false); // This line should not be reached if validation throws an error
+    } catch (err) {
+        expect(err.errors['TemplateName']).toBeDefined();
+    }
+});
+
+test('should be falsy for missing TemplateStructure', async () => {
+    const formTemplate = new FormTemplate({
+        TemplateName: 'Another Sample Form Template'
+    });
+
+    try {
+        await formTemplate.validate();
+        expect(true).toBe(false); // This line should not be reached if validation throws an error
+    } catch (err) {
+        expect(err.errors['TemplateStructure']).toBeDefined();
+    }
+});
