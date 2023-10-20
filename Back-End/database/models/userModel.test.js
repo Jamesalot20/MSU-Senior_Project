be4e5c3b4ed87d4ea5c1530bee593e397ff135da
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-10-20 12:22:59 -0500

    Update userModel.test.js

--- Back-End/database/models/userModel.test.js
+++ Back-End/database/models/userModel.test.js
@@ -9,3 +9,28 @@ test('should be truthy for valid user', () => {
 
     expect(user.Name).toBe('testUser');
 });
+test('should be falsy for an invalid user without email', () => {
+    const user = new User({
+        Name: 'Test Name',
+        Password: 'testPass'
+    });
+
+    // Try saving and expect a validation error
+    user.save(err => {
+        expect(err).toBeTruthy();
+        expect(err.errors['Email']).toBeDefined();
+    });
+});
+
+test('should be falsy for an invalid user without password', () => {
+    const user = new User({
+        Name: 'Test Name',
+        Email: 'test@email.com'
+    });
+
+    // Try saving and expect a validation error
+    user.save(err => {
+        expect(err).toBeTruthy();
+        expect(err.errors['Password']).toBeDefined();
+    });
+});
